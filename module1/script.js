be14f8d6f9501d33e4d1ad6896a9a211363d6b47
gleb <glebaz666@gmail.com>
2018-06-25 00:44:00 +0300

    add

---
+++ module1/script.js
@@ -0,0 +1,72 @@
+
+const ADMIN_LOGIN = 'admin';
+const ADMIN_PASSWORD = '123';
+
+
+// let passwordEnter;
+let loginEnter = prompt('Введите ваш логин:');
+
+
+if (loginEnter === null) {
+    alert('Cancel') 
+}   else if(loginEnter !== ADMIN_LOGIN) {
+    alert('Доступ запрещен!')
+}     else {
+    let passwordEnter = prompt('Введите ваш пароль:')
+}
+
+if (passwordEnter === null) {
+    alert('Cancel')
+}  else if(passwordEnter !== ADMIN_PASSWORD) {
+    alert('Доступ запрещен!')
+} else {
+    alert('Welocome ' + ADMIN_LOGIN) 
+}
+
+const ADMIN_LOGIN = 'admin';
+const ADMIN_PASSWORD = '123';
+
+
+// let passwordEnter;
+let loginEnter = prompt('Введите ваш логин:');
+
+
+if (loginEnter === null) {
+    alert('Cancel') 
+}   else if(loginEnter !== ADMIN_LOGIN) {
+    alert('Доступ запрещен!')
+}     else {
+    let passwordEnter = prompt('Введите ваш пароль:')
+    if (passwordEnter === null) {
+        alert('Cancel')
+    }  else if(passwordEnter !== ADMIN_PASSWORD) {
+        alert('Доступ запрещен!')
+    } else {
+        alert('Welocome ' + ADMIN_LOGIN) 
+    }
+}
+
+// const ADMIN_LOGIN = 'admin';
+// const ADMIN_PASSWORD = '123';
+// const cancel = 'Отменено пользователем!';
+// const ban = 'Доступ запрещен!';
+// const welcome = 'Добро пожаловать!';
+
+
+// const log = prompt('Введите ваш логин');
+
+// if (log === null) {
+//     alert(cancel);
+// } else if (log !== ADMIN_LOGIN) {
+//     alert(ban);
+// } else {
+//     let pass = prompt('Введите пароль');
+
+//     if (pass === null) {
+//         alert(cancel);
+//     } else if (pass !== ADMIN_PASSWORD) {
+//         alert(ban);
+//     } else {
+//         alert(welcome);
+//     }
+// }
