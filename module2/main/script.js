cdff35aeb536f9d420b7222749f149b9a6f9c1ee
gleb <glebaz666@gmail.com>
2018-06-28 10:28:44 +0300

    module2

---
+++ module2/main/script.js
@@ -0,0 +1,37 @@
+let userInput;
+const numbers = [];
+let total = 0;
+
+do {
+    let userInput = prompt('sx')
+    if (userInput === null){
+        break;
+    }
+    console.log(userInput);
+    
+} while (true)
+// ------------------------------homework------------------------------
+
+
+
+// let a = 100;
+// let arr = [100, 50, 25];
+
+// let b = a;
+// let bar = arr;
+
+// b = 200;
+// bar[0] = 200;
+
+// console.log(`a = ${a}`);
+// console.log(`arr[0] = ${arr[0]}`);
+
+// const prime = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+// let sum = 0;
+
+// for (let i = 0; i < prime.length; i += 1) {
+//     sum += prime[i]
+// }
+
+// console.log(sum);
