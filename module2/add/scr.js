5c1ef8dd5fd70bd91bff8d6fbc725e4b6f152b1a
gleb <glebaz666@gmail.com>
2018-07-12 11:52:17 +0300

    module2

--- module2/add/scr.js
+++ module2/add/scr.js
@@ -1,67 +1,159 @@
 // --------------------------------1--------------------------------
-// const users = ['Mango', 'Poly', 'Ajax', 'Chelsey'];
+const users = ['Mango', 'Poly', 'Ajax', 'Chelsey'];
 
-// console.log(users.length);
+console.log(users.length);
 
-// console.log(users[0]); 
-// console.log(users[1]); 
-// console.log(users[2]); 
-// console.log(users[3]); 
+console.log(users[0]); 
+console.log(users[1]); 
+console.log(users[2]); 
+console.log(users[3]); 
 // --------------------------------1--------------------------------
 
 // --------------------------------2--------------------------------
 
-// const users = ["Mango", "Poly", "Ajax", "Chelsey"];
+const users = ["Mango", "Poly", "Ajax", "Chelsey"];
 
-// /* Используя методы массива, последовательно выполнить следующие операции */
 
-// // Удалить из начала массива нулевой элемент
+console.log(users.shift());
 
-// console.log(users.shift());
+console.log(users.pop()); 
 
-// // Удалить из конца массив последний элемент
-// console.log(users.pop()); 
-// users.unshift ("добавленое имя");
-// // Добавить в начало массива любое имя
-// console.log(users);
-// // Добавить в конец массива два любых имени за одну операцию
-// users.splice(3, 0, "имя 1", "имя 2")
-// console.log(users);
+users.unshift ("добавленое имя");
+console.log(users);
+
+users.splice(3, 0, "имя 1", "имя 2")
+console.log(users);
 
 // --------------------------------2--------------------------------
 
 // --------------------------------3--------------------------------
+let arr = [];
+let string = prompt('Введите что-то: ');
+// Разбить строку в массив, пусть разделителем будет пробел, и записать в переменную arr
+console.log(arr = string.split(" "));
+// Вывести в консоли общую длину массива arr
+console.log("arr length:", arr.length);
+// Используя цикл, вывести в консоль все индексы массива arr
 
-let string;
-
-do {
-    string = prompt('Введите произвольное число');
-} while (string === null);
-
-
+// Используя цикл, вывести в консоль все элементы массива arr
+let el = [];
+for (el of arr) {
+    console.log(el);
+}
+// Используя цикл, bывести в консоли все пары индекс:значение массива arr
 
 // --------------------------------3--------------------------------
 
 // --------------------------------4--------------------------------
 
-// let counter = 0;
+let counter = 0;
 
-// while (counter < 10) {
-//   console.log("counter: ", counter);
+while (counter < 10) {
+  console.log("counter: ", counter);
 
-//   counter += 1;
-// }
+  counter += 1;
+}
 
+let input;
+do {
+    input = prompt('Введите число больше 100', '');
 
-// let input = 0;
+    if (input < 100){
+        alert('Введить можно только числа, больше 100')
+        }
+        if (input == null){
+            alert('Приходите еще!')
+            }
+    console.log(input);
+    
+    
+} while(input <= 100);
 
-// do {
-//   input = Number (prompt('Введите число больше 100', ''));
-//   console.log(input);
-  
-// } while(input <= 100 || input === null);
 
 // --------------------------------4--------------------------------
 
+// --------------------------------5--------------------------------
+
+const min = 1;
+const max = 100;
+
+for (let i = 0; i <= max; i++){
+    if(i % 3){
+        console.log('Fizz');
+    }
+    if (i % 5){
+        console.log('Buzz');
+        }
+        console.log(i);
+        
+}
+
+// --------------------------------5--------------------------------
+
+// --------------------------------6--------------------------------
+
+const numbers = [1, 3, 17, 5, 9, 14, 8, 14, 34, 18, 26];
+const num = 10;
+const newArray = [];
+
+for (i = 0; i <= numbers.length -1; i++){
+    if(numbers[i] > num){
+        newArray.push(numbers[i])
+    }
+}   console.log(newArray);
+
+// --------------------------------6--------------------------------
+
+// --------------------------------7--------------------------------
+
+const string = "May the force be with you";
+let longestWord = '';
 
 
+let strSplit = string.split(' ');
+console.log(strSplit);
+
+for(let i = 0; i < strSplit.length -1; i++){
+    if(strSplit[i].length > longestWord.length){
+        longestWord = strSplit[i];
+    }
+}
+    console.log(longestWord); // 'force'
+
+// --------------------------------7--------------------------------
+
+// --------------------------------8--------------------------------
+
+let string;
+let arr = [];
+let summ = 0;
+let integerArr = [];
+
+do {
+    string = prompt('Введите число');
+    arr.push(string);
+    if (string === null || string === ' ' || string === '') break;
+} while(string % 1 == 0)
+
+arr.pop()
+
+for(let i = 0; i <= arr.length - 1; i++) {
+    integerArr.push(arr[i]);
+}
+console.log(integerArr)
+
+for (let j = 0; j <= integerArr.length - 1; j++) {
+    summ = summ + parseInt(integerArr[j]);
+}
+console.log('Сумма чисел: ',summ)
+
+// --------------------------------8--------------------------------
+
+// --------------------------------9--------------------------------
+
+// const numbers = [12, 15, 25, 37, 41];
+// let input;
+
+
+
+// --------------------------------9--------------------------------
--- module2/main/script.js
+++ module2/main/script.js
@@ -3,35 +3,40 @@ const numbers = [];
 let total = 0;
 
 do {
-    let userInput = prompt('sx')
-    if (userInput === null){
-        break;
-    }
-    console.log(userInput);
+    userInput = prompt('Введите число')
+    if( userInput % 1 !== 0 || userInput === '' || userInput === ' ') {
+        alert('Было введено не число, попробуйте еще раз');
+      } else {
+        numbers.push(userInput);
+      }
+    }while (userInput !== null)
     
-} while (true)
+    numbers.pop();
+    console.log(numbers);
+    
+    for (let item of numbers) {
+      total += numbers;
+    }
+    alert ('Общая сумма чисел равна ${total}');
 // ------------------------------homework------------------------------
 
 
 
-// let a = 100;
-// let arr = [100, 50, 25];
+const passwords = ['qwerty', '111qwe', '123123', 'r4nd0mp4zzw0rd'];
+let attempts = 3;
+let passwordInput;
+let i = 0;
+let k = attempts - 1;
 
-// let b = a;
-// let bar = arr;
-
-// b = 200;
-// bar[0] = 200;
-
-// console.log(`a = ${a}`);
-// console.log(`arr[0] = ${arr[0]}`);
-
-// const prime = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-
-// let sum = 0;
-
-// for (let i = 0; i < prime.length; i += 1) {
-//     sum += prime[i]
-// }
-
-// console.log(sum);
+do {
+  passwordInput = prompt('Введите пароль');
+  if (passwords.includes(passwordInput) === true) {
+    alert('Добро пожаловать!');
+    break;
+  }else if (i === 2) {
+    alert('У вас закончились попытки, аккаунт заблокирован!')
+  }else {
+    alert(`Неверный пароль, у вас осталось ${k--} попыток`);
+  }
+  i++
+}while (i < attempts);
