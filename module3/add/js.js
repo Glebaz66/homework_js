7142780114c648e63c29e6d84d2578adaac46f1f
Unknown <glebaz666@gmail.com>
2018-07-18 23:00:17 +0300

    mod3 add

---
+++ module3/add/js.js
@@ -0,0 +1,25 @@
+// --------------------------------1--------------------------------------
+
+// function checkNumberType(num) {
+//     return num % 2 === 0 ? "Odd" : "Even"
+// }
+
+// console.log( checkNumberType(2) ); // 'Even'
+
+// console.log( checkNumberType(46) ); // 'Even'
+
+// console.log( checkNumberType(3) ); // 'Odd'
+
+// console.log( checkNumberType(17) ); // 'Odd
+
+// --------------------------------1--------------------------------------
+
+// --------------------------------2--------------------------------------
+function formatString(str) {
+    return console.log(
+        formatString("Curabitur ligula sapien, tincidunt non.")
+      );
+}
+
+    formatString()
+// --------------------------------2--------------------------------------
