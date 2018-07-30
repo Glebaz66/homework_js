096481f4fd82f6f9f2311026cf8e5fbf059a993c
unknown <glebaz666@gmail.com>
2018-07-30 15:51:50 +0300

    upd

---
+++ module4/main/js.js
@@ -0,0 +1,35 @@
+const products = {
+    bread: 10,
+    milk: 15,
+    apples: 20,
+    chicken: 50,
+    cheese: 40,
+};
+
+const totalPrice = 0;
+const change = 0;
+
+const Cashier = function (name, productDatabase, customerMoney){
+    this.name = name;
+    this.productDatabase = productDatabase;
+    this.customerMoney = customerMoney;
+
+    this.getCustomerMoney = function (value){
+        return
+    }
+    this.countTotalPrice = function (order) {
+        return
+    }
+    this.countChange = function (totalPrice) {
+        return
+    }
+    this.onSuccess = function (change) {
+        return
+    }
+    this.onError = function (){
+        return
+    }
+    this.reset = function (){
+        return
+    }
+} 
