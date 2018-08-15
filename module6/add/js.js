d00d47d4dbebe3e864f8da0d72d35491d766f065
Azimov Gleb <glebaz666@gmail.com>
2018-08-15 20:27:48 +0300

    add mod 6

--- module6/add/js.js
+++ module6/add/js.js
@@ -1,6 +1,60 @@
-const obj = {
-  name: 'name',
-}
+// -------------------------------1-------------------------------
 
-console.log(obj);
-console.log(obj.hasOwnProperty('x'));
+// function Account(login, email, friendsCount){
+//     this.login = login;
+//     this.email = email;
+//     this.friendsCount = friendsCount;
+
+//     this.getAccountInfo = function(){
+//         console.log("login is ", this.login);
+//         console.log('email is', this.email);
+//         console.log('friendsCount is', this.friendsCount);
+//     }
+// }
+
+// const ivan = new Account('Ivan', 'dfsf@sdfsdf.com', 3);
+
+// ivan.getAccountInfo();
+
+
+// -------------------------------1-------------------------------
+
+// -------------------------------2-------------------------------
+
+function StringBuilder(string = "") {
+    this.value = string;
+
+    this.getValue = function getValue(){
+        console.log(myString);
+    }
+    this.append = function append(){
+
+    }
+    this.prepend = function prepend(){
+
+    }
+    this.append = function pad(){
+
+    }
+  }
+  
+  
+  const myString = new StringBuilder('.');
+  getValue(myString);
+  
+  myString.append('^'); 
+  myString.showValue(); // '.^'
+  
+  myString.prepend('^'); 
+  myString.showValue(); // '^.^'
+  
+  myString.pad('='); 
+  myString.showValue(); // '=^.^='
+  
+// -------------------------------2-------------------------------
+
+// -------------------------------2-------------------------------
+
+
+
+// -------------------------------2-------------------------------
