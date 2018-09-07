5b09a2135479c5d584efd35715cdbff665e40243
gleb <glebaz666@gmail.com>
2018-09-07 15:47:58 +0300

    ad

--- module9/main/js.js
+++ module9/main/js.js
@@ -1,31 +1,61 @@
-const startBtn = document.querySelector('.js-start');
-const takeLapBtn = document.querySelector('.js-take-lap');
-const resetBtn = document.querySelector('.js-reset');
-const saveLap = document.querySelector('.js-laps');
-const timeDigits = document.querySelector('.js-time');
+const startBtn = document.querySelector('.js-start'); // кнопка старт
+const takeLapBtn = document.querySelector('.js-take-lap'); // кнопка для считывание текущего времени
+const resetBtn = document.querySelector('.js-reset'); // сброс
+const timeDigits = document.querySelector('.js-time'); // 
+const lapsTable = document.querySelector('.js-laps'); //
 
 const timer = {
     id: null,
     startTime: null,
+    isActive: false,
+    isPaused: false,
     start(){
         this.startTime = Date.now();
+        this.isActive = true;
+        this.isPaused = false;
         this.id = setInterval(() => {
             const currentTime = Date.now();
             const deltaTime = currentTime - this.startTime;
-            // console.log(deltaTime);
-            updateClockface(deltaTime);
-            
-        }, 1)
+            updateClockface(deltaTime); 
+
+            if(timer.isActive){
+                startBtn.textContent = 'Pause';
+            } 
+            if(startBtn.textContent === 'Pause') {
+                startBtn.textContent = 'Continue';
+            }
+        }, 100)
     },
+
     reset(){
         clearInterval(this.id);
         const resetTime = this.startTime = '00:00.0';
+        // const resetTime = timer.startTime;
+        // this.deltaTime = 0;
         timeDigits.textContent = resetTime;
-    }
+        // document.querySelector('.js-time').nodeValue
+        console.log(document.querySelector('.js-time').typeOf);
+        
+    },
+
+    lap(){
+        const lapArr = [];
+        lapArr.push(document.querySelector('.js-time').textContent);
+
+        const lists = lapArr.map(el => {
+            list = document.createElement('li');
+            list.className = 'laps__item';
+            list.textContent = el;
+            return list;
+        })
+        document.querySelector('.laps').appendChild(...lists);
+    },
+
 }
 
 startBtn.addEventListener('click', timer.start.bind(timer));
 resetBtn.addEventListener('click', timer.reset.bind(timer));
+takeLapBtn.addEventListener('click', timer.lap.bind(timer));
 
 function updateClockface (time) {
     const formattedTime = formatTime(time);
