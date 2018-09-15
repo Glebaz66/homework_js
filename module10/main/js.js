e7a327fe491299693b839bd470508abc0b3dfbee
gleb <glebaz666@gmail.com>
2018-09-15 20:50:29 +0300

    addd

--- module10/main/js.js
+++ module10/main/js.js
@@ -9,31 +9,31 @@ const table = document.querySelector('table');
 const input = document.querySelector('input');
 
 
-// function getAllUsers(evt){
-//     evt.preventDefault();
+function getAllUsers(evt){
+    evt.preventDefault();
 
-//     fetch('https://test-users-api.herokuapp.com/users/')
-//     .then(resp => {
-//         if(resp.ok){
-//             return resp.json();
-//         }
-//         throw new Error('fetch error');
-//     })
-//     .then(data => {
-//         const usersListData = data.data.reduce((acc, elem) => acc + 
-//         `<tr class="list-users-row">
-//             <td>${elem.age}</td>
-//             <td>${elem.name}</td>
-//             <td>${elem.id}</td>
-//         </tr>
-//         `, '')
-//         result.innerHTML = `${usersListData}`;
-//         table.style.display = 'block';
-//     })
-//     .catch(error => {
-//         console.log('error2', error);
-//     })
-// }
+    fetch('https://test-users-api.herokuapp.com/users/')
+    .then(resp => {
+        if(resp.ok){
+            return resp.json();
+        }
+        throw new Error('fetch error');
+    })
+    .then(data => {
+        const usersListData = data.data.reduce((acc, elem) => acc + 
+        `<tr class="list-users-row">
+            <td>${elem.age}</td>
+            <td>${elem.name}</td>
+            <td>${elem.id}</td>
+        </tr>
+        `, '')
+        result.innerHTML = `${usersListData}`;
+        table.style.display = 'block';
+    })
+    .catch(error => {
+        console.log('error2', error);
+    })
+}
 
 function getUserById(evt){
     evt.preventDefault();
@@ -46,21 +46,20 @@ function getUserById(evt){
         throw new Error('fetch error');
     })
     .then(data => {
+        // console.log(data.data[0].id);
         
         const inputValue = input.value;
         const id = data.data.find(elem => elem.id === inputValue);
-        
-        const userId = document.createElement('div');
-        userId.className('.user-id-text');
-        console.log(userId);
-        
-        return userId.appendChild(user-id-text) += `${id}`;
-        
+        const userId = document.querySelector('.user-id');
+        console.log('id',id.id);
+        if(inputValue == id) {
+            return userId.innerHTML = `Personal id: ${id}`;
+        } else {return null};
     })
     .catch(error => {
         console.log('Person not found', error);
         
     })
 }
-// usersList.addEventListener('click', getAllUsers);
+usersList.addEventListener('click', getAllUsers);
 userId.addEventListener('click', getUserById);
