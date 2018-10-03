2239d25cbbdbdc59bdeea917d8b98b0bd4e7900a
glebaz66 <glebaz666@gmail.com>
2018-10-03 21:22:01 +0300

    add

--- module11/add/js.js
+++ module11/add/js.js
@@ -1,11 +1,27 @@
-const listItems = [
-    { name: 'item 1', count: 2 },
-    { name: 'item 2', count: 4 },
-    { name: 'item 3', count: 12 },
-    { name: 'item 4', count: 29 },
-];
-const list = document.querySelector('.list').innerHTML.trim();
-const template = Handlebars.compile(source);
-const markup = listItems.reduce((acc, el) => acc + template(el,), '');
+// -------------------------------------1---------------------------------------
+// const lists = document.querySelector(".list").innerHTML.trim();
+// const template = Handlebars.compile(lists);
+// const listItems = [
+//     { name: "item 1", count: 2 },
+//     { name: "item 2", count: 4 },
+//     { name: "item 3", count: 12 },
+//     { name: "item 4", count: 29 }
+// ];
+// const markup = listItems.reduce((acc, list) => acc + template(list), "");
+// document.querySelector('.listRoot').insertAdjacentHTML('afterbegin', markup) ;
+// -------------------------------------1---------------------------------------
+// -------------------------------------2---------------------------------------
+const posts = [
+    { title: "post 1", text: "text 1", isFav: true },
+    { title: "post 2", text: "text 2", isFav: false },
+    { title: "post 3", text: "text 3", isFav: true },
+    { title: "post 4", text: "text 4", isFav: false }
+  ];
+const postsData = document.querySelector('.post').innerHTML.trim();
+const postTmp = Handlebars.compile(postsData);
+const postMarkup = posts.reduce((acc, post) => acc + postTmp(post), '');
+document.querySelector('.post-root').innerHTML = postMarkup;
+// -------------------------------------2---------------------------------------
+// -------------------------------------3---------------------------------------
+// -------------------------------------3---------------------------------------
 
-list.insertAdjacentHTML('afterbegin', template);
