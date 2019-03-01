87442a706cfcca3038d61e101ea76edbf17f7ac7
gleb <glebaz666@gmail.com>
2019-03-01 13:00:44 +0200

    time backcount add

--- game/scr.js
+++ game/scr.js
@@ -1,31 +1,38 @@
 const cards = document.querySelectorAll(".card");
 const restart = document.querySelector("button[type=reset]");
-const time = document.querySelector(".js-couter-time");
 
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
 
+
+startGame = () => {
+  card.addEventListener("click", timerStart);
+  return console.log(cards);
+  
+}
+console.log(startGame);
+
 function flipCard() {
   if (lockBoard) return;
   if (this === firstCard) return;
-
+  
   this.classList.add("flip");
-
+  
   if (!hasFlippedCard) {
     hasFlippedCard = true;
     firstCard = this;
-
+    
     return;
   }
-
+  
   secondCard = this;
   checkForMatch();
 }
 
 function checkForMatch() {
   let isMatch = firstCard.dataset.char === secondCard.dataset.char;
-
+  
   isMatch ? disableCards() : unflipCards();
 }
 
@@ -36,17 +43,17 @@ function resetBoard() {
 function disableCards() {
   firstCard.removeEventListener("click", flipCard);
   secondCard.removeEventListener("click", flipCard);
-
+  
   resetBoard();
 }
 
 function unflipCards() {
   lockBoard = true;
-
+  
   setTimeout(() => {
     firstCard.classList.remove("flip");
     secondCard.classList.remove("flip");
-
+    
     resetBoard();
   }, 1000);
 }
@@ -60,22 +67,26 @@ function unflipCards() {
 
 restartBtn = () => {
   window.location.reload();
-  // document.querySelector('.card.flip');
-  // if(firstCard.nodeName || secondCard.classList == 'flip'){
-  //   resetBoard();
-  // }
 };
 
 cards.forEach(card => card.addEventListener("click", flipCard));
 restart.addEventListener("click", restartBtn);
 
 // ----------timer-------------
-let startCounter;
-function timer(){
-  startCounter = time.innerHTML;
-  time.innerHTML --
-  setInterval(1000);
+const time = document.querySelector(".js-couter-time");
+console.log(time);
+
+function timerStart() {
+  let currentCount = 30;
+  const startCounter = setInterval(function() {
+    time.textContent = `Time left: ${currentCount}`
+    if (currentCount == 0) clearInterval(startCounter);
+    currentCount--;
+    
+  }, 1000);
 };
-console.log(time.innerHTML);
+cards.addEventListener('click', timerStart);
+console.log(cards);
+
 
 // ----------timer-------------
