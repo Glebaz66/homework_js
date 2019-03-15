c2a310e70c3cb94d091fc9e5249bc24dec2ae4cd
gleb <glebaz666@gmail.com>
2019-03-15 17:26:45 +0200

    add counddown, lose message

--- game/scr.js
+++ game/scr.js
@@ -8,23 +8,18 @@ let firstCard, secondCard;
 function flipCard() {
   if (lockBoard) return;
   if (this === firstCard) return;
-  
   this.classList.add("flip");
-  
   if (!hasFlippedCard) {
     hasFlippedCard = true;
     firstCard = this;
-    
     return;
   }
-  
   secondCard = this;
   checkForMatch();
 }
 
 function checkForMatch() {
-  let isMatch = firstCard.dataset.char === secondCard.dataset.char;
-  
+  const isMatch = firstCard.dataset.char === secondCard.dataset.char;
   isMatch ? disableCards() : unflipCards();
 }
 
@@ -67,17 +62,16 @@ restart.addEventListener("click", restartBtn);
 // ---get ready
 const getReadyTitle = document.querySelector('.get-ready');
 
-let getReadyTitleColor = function() {
+const getReadyTitleColor = function() {
   return Math.floor(Math.random() * 255)
 };
-let getReadyTitleSize = function() {
+const getReadyTitleSize = function() {
   if(getReadyTitle.style.fontSize == '50' + 'px'){
     getReadyTitle.style.fontSize = '45' + 'px';
   }
   else {
     getReadyTitle.style.fontSize = '50' + 'px';
   }
-    return getReadyTitle.style.fontSize;
 };
 setInterval(function() {
   getReadyTitle.style.color = 'rgb(' + getReadyTitleColor() + ',' + getReadyTitleColor() + ',' + getReadyTitleColor() + ')';
@@ -94,23 +88,50 @@ setTimeout(() => {
 // ---get ready
 // ----------timer-------------
 const time = document.querySelector(".js-couter-time");
+time.textContent = 'Time left: 30 seconds';
 setTimeout(() => {
   (function timerStart() {
-    let currentCount = 31;
+    let currentCount = 30;
     const startCounter = setInterval(function() {
       if (currentCount === 1) clearInterval(startCounter);
       currentCount--;
-      // console.log(time.textContent);
       return time.textContent = `Time left: ${currentCount} seconds` ;
     }, 1000); 
   })();
 }, 5000) //delay for starting countdown for 30sec
 
 // ----------timer-------------
-const stopGame = () =>{
-  if(time.textContent === 'Time left: 0 seconds'){
-   return resetBoard();
-  } 
+const stopGame = function(){
+ const lose = document.querySelector('.lose-popup');
+ 
+ setTimeout(() => {
+   if(time.textContent === 'Time left: 0 seconds'){
+     lose.style.display = 'flex';
+    }
+  }, 36000);
+}();
 
-}
+// const win = document.querySelector('.win-popup'); in progress
+
+// ===============================
+// moves count
+
+// let click = '';
+const img = document.querySelectorAll('.back');
+let click = '';
+console.log(img);
+
+const moveCount = function(img) {
+  e.target = '';
+  e.preventDefault();
+  let currentCount;
+  if(img.classList === 'back'){
+    currentCount++;
+    console.log(currentCount);
+    
+  }
+  return console.log(e.target);
   
+}
+click.addEventListener("click", moveCount);
+
